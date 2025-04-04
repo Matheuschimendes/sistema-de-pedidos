f097d92bd0e164f747a52950a90a0eb54005b3ed
Matheus Chimendes <matheuschimendes@MacBook-Air-di-Matheus.local>
2025-04-04 20:07:55 -0300

    inclution

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return (<h1 className=" border-collapse rounded-md  text-red-300 font-bold">Hello world</h1>);
+}
+ 
+export default HomePage ;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,5 @@
+const ProductPages = () => {
+  return <h1>Products Pages</h1>
+};
+ 
+export default ProductPages;
